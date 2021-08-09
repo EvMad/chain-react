3d1f8eef9f748d15da56b4aa913cdf56a97c2fe9
GitPit <rifflifter@gmail.com>
2021-08-09 19:56:01 -0400

    added <a link divs to NavBar.js using {LINK}

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,11 @@ class NavComponent extends Component {
     render() {
         return (
             <div>
+                <nav>
+                    <a as={Link} to="/about">About</a>
+                    <a as={Link} to="/contact">Contact</a>
+                    <a as={Link} to="/projects">Portfolio</a>
+                </nav>
 
             </div>
         )
