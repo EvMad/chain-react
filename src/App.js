1ec6093dd5ea51d77794e21493002f8a3d036223
GitPit <rifflifter@gmail.com>
2021-08-09 18:35:08 -0400

    added imports to App.js, switch/Router divs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProjectBox from './components/ProjectBox';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
@@ -6,9 +8,23 @@ import About from './components/About';
 import Contact from './components/Contact';
 
 function App() {
+    const renderNavFooter = component => {
+        return (
+            <NavBar />
+            {component}
+            <Footer />
+        )
+    }
+
     return (
-        <div className="chain-app">
-            <ProjectBox />
+        <div style={{ marginBottom: '30px', marginTop: '0px', paddingBottom: '40px'}}>
+            <Router>
+                <div className="chain-app">
+                    <Switch>
+                        <Route path="/about"></Route>
+                    </Switch>
+                </div>
+            </Router>
         </div>
     )
 }
