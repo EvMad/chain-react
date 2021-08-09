cd8d98050d7d823e1f7a305ad02c097d786f4cfd
GitPit <rifflifter@gmail.com>
2021-08-09 18:06:16 -0400

    created App.js, App.css, ChainBox.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ChainBox from './components/ChainBox';
+
+function App() {
+    return (
+        <div className="chain-app">
+            <ChainBox />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/ChainBox.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+function ChainBox() {
+    const [box, setBox] = useState([]);
+}
