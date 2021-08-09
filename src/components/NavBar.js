777bb7a5d9ac1770c274b2dd9087a1cb9c8a42f3
GitPit <rifflifter@gmail.com>
2021-08-09 18:50:49 -0400

    created NavComponent and div outline for NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,2 +1,14 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+
+class NavComponent extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default NavComponent;
