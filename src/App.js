a30b1a1c661f687f6555f38736a27f33acbb41c1
GitPit <rifflifter@gmail.com>
2021-08-12 18:23:19 -0400

    removed component function

--- src/App.js
+++ src/App.js
@@ -8,29 +8,30 @@ import About from './components/About';
 import Contact from './components/Contact';
 
 function App() {
-    const renderNavFooter = component => {
+
+
+    
         return (
             <>
-            <NavBar />
-            { component }
-            <Footer />
-            </>
-        )
-    }
-
-    return (
-        <div style={{ marginBottom: '30px', marginTop: '0px', paddingBottom: '40px'}}>
+            
+            <div style={{ marginBottom: '30px', marginTop: '0px', paddingBottom: '40px'}}>
             <Router>
+            <NavBar />
                 <div className="chain-app">
                     <Switch>
-                        <Route path="/about">{renderNavFooter(<About />)}</Route>
-                        <Route path="/projects">{renderNavFooter(<ProjectBox />)}</Route>
-                        <Route path="/contact">{renderNavFooter(<Contact />)}</Route>
+                    {/* <Route path="/">{renderNavFooter(<About />)}</Route> */}
+                        <Route path="/about">{<About />}</Route>
+                        <Route path="/projects">{<ProjectBox />}</Route>
+                        <Route path="/contact">{<Contact />}</Route>
                     </Switch>
                 </div>
+                <Footer />
             </Router>
         </div>
-    )
-}
+           
+            </>
+        )
+        }
+
 
 export default App;
--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -5,24 +5,7 @@ class ProjectBox extends Component {
         return (
             <div>
            
-            <div class="image">
-                <img src="./assets/images/1.jpg" alt="">
-                <div class="details">
-                    <h2>Lou<span>ReadMe</span></h2>
-                    <p>Node.js README Generator</p>
-                    <div class="more">
-                        <a href="https://evmad.github.io/lou-reed-me/" target="_blank" rel="noopener noreferrer"
-                            class="read-more">Read<span>More</span></a>
-                        <div class="icon-links">
-
-                            <a href="https://evmad.github.io/lou-reed-me/" target="_blank" rel="noopener noreferrer"><i
-                                    class="fas fa-eye"></i></a>
-
-                        </div>
-                    </div>
-                </div>
-            </img>
-            </div>
+        
             </div>
         )
        
