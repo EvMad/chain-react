9227d8c3838719eb41aa3c3bbe42b15c39d60d7e
GitPit <rifflifter@gmail.com>
2021-08-12 22:41:03 -0400

    added contact form / submit button is off center

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
+import './contact.css';
+
+
 
 class Contact extends Component {
     render() {
         return (
             <div>
-            <form>
-  <label>
+                <div className="column">
+                <form style={{ display: "flex", flexDirection: "column", alignContent: "center"}}>
+                <label>
     Name:
-    <input type="text" name="name" />
+    <input type="text" placeholder="your name here" name="name" style={{ alignContent: "flex-start", minWidth: "30%", minHeight: "50px"}} />
   </label>
-  <input type="submit" value="Submit" />
-</form>
+  <label>
+    Email:
+    <input type="text" placeholder="email@email.com" name="name" style={{ alignContent: "flex-start", minWidth: "30%", minHeight: "50px"}} />
+  </label>
+        <label>
+          Essay:
+          <textarea placeholder="your message here" style={{ alignContent: "flex-start", minWidth: "30%", minHeight: "300px"}} />
+        </label>
+        <input style={{ maxWidth: "10%", minHeight: "50px" }}  type="submit" value="Submit" />
+      </form>
+      </div>
             </div>
         )
        
