dc17e1b3144f0bc16713062bbb82c3f5dc2fa767
GitPit <rifflifter@gmail.com>
2021-08-12 22:10:53 -0400

    added name to header/nav

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,13 @@ class Contact extends Component {
     render() {
         return (
             <div>
-            
+            <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
             </div>
         )
        
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,6 +10,7 @@ function NavBar() {
                 
                <nav>
                    <ul style={{ listStyle: 'none', display: 'flex', justifyContent: 'Space-Around' }}>
+                       <li style={{ color: '#fff', textDecoration: 'none', fontFamily: 'Merriweather', fontWeight: 900 }}> Evan Madden</li>
                        <li><Link style={{ color: '#fff', textDecoration: 'none', fontFamily: 'Merriweather', fontWeight: 900 }} to="/about">About</Link></li>
                        <li><Link style={{ color: '#fff', textDecoration: 'none', fontFamily: 'Merriweather', fontWeight: 900 }} to="/contact">Contact</Link></li>
                        <li><Link style={{ color: '#fff', textDecoration: 'none', fontFamily: 'Merriweather', fontWeight: 900 }} to="/projects">Portfolio</Link></li>
