e9068481161f7f42846d516d5e7e1e7349316f2b
GitPit <rifflifter@gmail.com>
2021-08-16 16:39:19 -0400

    added new images and updated links for portfolio

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import './ProjectBox.css';
 import imgOne from './images/1.jpg';
+import noteBook from './images/notebook.jpg';
+import cat from './images/cat.jpg';
+import stage from './images/stage.jpg';
+import paint from './images/paintCan.jpg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
@@ -17,7 +21,7 @@ class ProjectBox extends Component {
            <div class="container">
                <div class="row">
                <div class="image">
-                <img src={imgOne} alt=""/>
+                <img src={cat} alt=""/>
                 <div class="details">
                     <h2>Git<span>Hub</span></h2>
                     <p></p>
@@ -33,15 +37,15 @@ class ProjectBox extends Component {
             </div>
             {/* image-card-end */}
             <div class="image">
-                <img src={imgOne} alt=""/>
+                <img src={stage} alt=""/>
                 <div class="details">
-                    <h2>Git<span>Hub</span></h2>
+                    <h2>Band<span>Finder</span></h2>
                     <p></p>
                     <div class="more">
-                        <a href="https://github.com/EvMad" target="blank" rel="noopener noreferrer"
-                            class="read-more">Git<span>Hub</span></a>
+                        <a href="https://keith-l-watford.github.io/artists-and-bands/" target="blank" rel="noopener noreferrer"
+                            class="read-more">Band<span>Finder</span></a>
                         <div class="icon-links">
-                            <a href="https://github.com/EvMad" target="_blank" rel="noopener noreferrer"><i><FontAwesomeIcon icon={['fas', 'eye']} /></i></a>
+                            <a href="https://keith-l-watford.github.io/artists-and-bands/" target="_blank" rel="noopener noreferrer"><i><FontAwesomeIcon icon={['fas', 'eye']} /></i></a>
 
                         </div>
                     </div>
@@ -49,16 +53,15 @@ class ProjectBox extends Component {
             </div>
             {/* image-card-end */}
             <div class="image">
-                <img src={imgOne} alt=""/>
+                <img src={paint} alt=""/>
                 <div class="details">
-                    <h2>Git<span>Hub</span></h2>
+                    <h2>Community<span>Canvas</span></h2>
                     <p></p>
                     <div class="more">
-                        <a href="https://github.com/EvMad" target="blank" rel="noopener noreferrer"
-                            class="read-more">Git<span>Hub</span></a>
+                        <a href="https://communitycanvas.herokuapp.com/" target="blank" rel="noopener noreferrer"
+                            class="read-more">Community<span>Canvas</span></a>
                         <div class="icon-links">
-                            <a href="https://github.com/EvMad" target="_blank" rel="noopener noreferrer"><i
-                                    class="fas fa-eye"></i></a>
+                            <a href="https://communitycanvas.herokuapp.com/" target="_blank" rel="noopener noreferrer"><i><FontAwesomeIcon icon={['fas', 'eye']} /></i></a>
 
                         </div>
                     </div>
@@ -66,16 +69,15 @@ class ProjectBox extends Component {
             </div>
             {/* image-card-end */}
             <div class="image">
-                <img src={imgOne} alt=""/>
+                <img src={noteBook} alt=""/>
                 <div class="details">
-                    <h2>Git<span>Hub</span></h2>
+                    <h2>Note<span>Taker</span></h2>
                     <p></p>
                     <div class="more">
-                        <a href="https://github.com/EvMad" target="blank" rel="noopener noreferrer"
-                            class="read-more">Git<span>Hub</span></a>
+                        <a href="https://calm-stream-38376.herokuapp.com/" target="blank" rel="noopener noreferrer"
+                            class="read-more">Dudely<span>Noted</span></a>
                         <div class="icon-links">
-                            <a href="https://github.com/EvMad" target="_blank" rel="noopener noreferrer"><i
-                                    class="fas fa-eye"></i></a>
+                            <a href="https://calm-stream-38376.herokuapp.com/" target="_blank" rel="noopener noreferrer"><i><FontAwesomeIcon icon={['fas', 'eye']} /></i></a>
 
                         </div>
                     </div>
