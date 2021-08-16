ad872515478f669c89762567e841604c51698422
GitPit <rifflifter@gmail.com>
2021-08-16 15:15:34 -0400

    added phone number to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ function Footer() {
 
 return (
     <div style={{ display: 'flex', justifyContent: 'Space-Around' }}>
-        <span style={{ color: '#fff' }} >copyright</span>
+        <span style={{ color: '#fff' }} >evan.madden@gmail.com</span>
+        <span style={{ color: '#fff' }} >267.314.7267</span>
     </div>
 );
 
