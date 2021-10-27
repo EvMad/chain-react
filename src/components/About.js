a4d98786483294397bea68fd09bd60aed247475e
GitPit <rifflifter@gmail.com>
2021-10-27 15:08:18 -0400

    changed bio text

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ class About extends Component {
                     <br></br>
                     <br></br>
                     
-            <p style={{ color: '#fff', fontFamily: 'Merriweather', fontWeight: 400 }}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            <p style={{ color: '#fff', fontFamily: 'Merriweather', fontWeight: 400 }}>Evan is a web developer with a music industry background, who has recently earned a Web Development certificate from the University of Pennsylvania LPS bootcamp, and is currently completing a UX Design Certificate through Google Coursera. His primary strengths are in organizing information, resources, and people. Evan’s technical strong points are in custom CSS,  CSS frameworks, vanilla Javascript, and React. Evan is currently pivoting his career to intersect web development and UX design with a skillset honed over a decade of managing client experience and event production.</p>
             </div>
            
             </div>
