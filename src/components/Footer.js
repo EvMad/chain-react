960baeb19a4e4242a04b76302b55c371e19213db
GitPit <rifflifter@gmail.com>
2021-11-15 15:32:20 -0500

    updated phone number

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
 return (
     <div style={{ display: 'flex', justifyContent: 'Space-Around' }}>
         <span style={{ color: '#fff' }} >evan.madden@gmail.com</span>
-        <span style={{ color: '#fff' }} >267.314.7267</span>
+        <span style={{ color: '#fff' }} >856.571.4345</span>
     </div>
 );
 
